d1bb8c5c2cf7622ecfaff6969916aa03fbb6b2ef
Kyle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2021-04-09 08:37:28 -0400

    fix tabbing

--- slashrequest.py
+++ slashrequest.py
@@ -6,26 +6,26 @@ from discord_slash import SlashCommand, utils
 def get(header, appID, guildID):
   url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
   head = f"Bot {header}"
-	headers = {
-	    "Authorization": head
-	}
-	f = requests.get(url, headers=headers)
-	return f
+  headers = {
+    "Authorization": head
+  }
+  f = requests.get(url, headers=headers)
+  return f
   
 def post(header, appID, guildID, jsonData):
   url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
   head = f"Bot {header}"
   headers = {
-		"Authorization": head
-	}
-	e = requests.post(url, headers=headers, json=jsonData)
+    "Authorization": head
+  }
+  e = requests.post(url, headers=headers, json=jsonData)
   return e
 
 def delete(header, appID, guildID, slashAppID):
   url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
   head = f"Bot {header}"
-	headers = {
-		"Authorization": head
-	}
-	r = requests.delete(url + f"/{slashAppID}", headers=headers)
-	return r
+  headers = {
+    "Authorization": head
+  }
+  r = requests.delete(url + f"/{slashAppID}", headers=headers)
+  return r
