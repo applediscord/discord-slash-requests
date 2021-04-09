527ce0935a05627e41520f159ede65187de0a89e
Kyle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2021-04-09 08:36:14 -0400

    fix spacing

--- slashrequest.py
+++ slashrequest.py
@@ -4,18 +4,18 @@ from discord.ext import commands
 from discord_slash import SlashCommand, utils
 
 def get(header, appID, guildID):
-		url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
-    head = f"Bot {header}"
-		headers = {
-		    "Authorization": head
-		}
-		f = requests.get(url, headers=headers)
-		return f
+  url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
+  head = f"Bot {header}"
+	headers = {
+	    "Authorization": head
+	}
+	f = requests.get(url, headers=headers)
+	return f
   
 def post(header, appID, guildID, jsonData):
   url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
   head = f"Bot {header}"
-	headers = {
+  headers = {
 		"Authorization": head
 	}
 	e = requests.post(url, headers=headers, json=jsonData)
