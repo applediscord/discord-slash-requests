a81c36fbf65b2fbd25e3f0bd8861540b456686c4
Kyle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2021-04-09 09:00:24 -0400

    eliminate flags in main functions

--- slashrequest.py
+++ slashrequest.py
@@ -1,10 +1,48 @@
 import requests
+import json
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, utils
 
-def get(header, appID, guildID):
-  url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
+def store(file, key=None, read=False, val=None):
+	with open(file, 'r') as v:
+		x = json.load(v)
+	if read is not False:
+		if key is None:
+			return x
+		else:
+			return x[key]
+	else:
+		x[key] = val
+		with open(file, 'w') as v:
+			json.dump(x, v, indent=4)
+
+# internal
+def checkURL():
+  x = store('config,json', 'slashID', True)
+  returns = []
+  if x['appID'] is not None:
+    returns.append(x['appID'])
+  
+  if x['guildID'] is not None:
+    returns.append(x['appID'])
+   
+  return returns
+
+def setURL(appID, guildID):
+  try:
+    x = store('config.json', None, True)
+    x['slashID']['appID'] = appID
+    x['slashID']['guildID'] = guildID
+    with open('config.json', 'r') as v:
+      json.dump(x, v, indent=4)
+    return True
+  except:
+    return False
+
+def get(header):
+  k = checkURL()
+  url = f"https://discord.com/api/v8/applications/{k[0]}/guilds/{k[1]}/commands
   head = f"Bot {header}"
   headers = {
     "Authorization": head
@@ -12,8 +50,9 @@ def get(header, appID, guildID):
   f = requests.get(url, headers=headers)
   return f
   
-def post(header, appID, guildID, jsonData):
-  url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
+def post(header, jsonData):
+  k = checkURL()
+  url = f"https://discord.com/api/v8/applications/{k[0]}/guilds/{k[1]}/commands"
   head = f"Bot {header}"
   headers = {
     "Authorization": head
@@ -21,8 +60,9 @@ def post(header, appID, guildID, jsonData):
   e = requests.post(url, headers=headers, json=jsonData)
   return e
 
-def delete(header, appID, guildID, slashAppID):
-  url = f"https://discord.com/api/v8/applications/{appID}/guilds/{guildID}/commands"
+def delete(header, slashAppID):
+  k = checkURL()
+  url = f"https://discord.com/api/v8/applications/{k[0]}/guilds/{k[1]}/commands"
   head = f"Bot {header}"
   headers = {
     "Authorization": head
